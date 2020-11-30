94b3a97f74413257a7dbd07f3ed5d055efab0437
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-30 15:59:12 -0500

    Create button class

---
+++ Card Workout (P)/CWButton.swift
@@ -0,0 +1,34 @@
+//
+//  CWButton.swift
+//  Card Workout (P)
+//
+//  Created by Ryan Spishock on 11/30/20.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor  = backgroundColor
+        setTitle(title, for: .normal)
+    }
+
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false     // use autolayout
+    }
+
+}
